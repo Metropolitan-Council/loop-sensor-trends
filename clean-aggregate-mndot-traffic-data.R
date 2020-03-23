691ef8bd7e2d4b196a322c867f923e8d38a4f7c9
Ashley Asmus <ashley.asmus@gmail.com>
2020-03-23 10:00:42 -0500

    minor edits, name changes

--- clean-aggregate-mndot-traffic-data.R
+++ clean-aggregate-mndot-traffic-data.R
@@ -2,9 +2,10 @@
 library(data.table)
 library(foreach)
 library(doParallel)
+library(lubridate)
 #############
 
-# READ DATA ####
+# READ DATA ----
 config <- fread('data/Configuration of Metro Detectors 2020-03-22.csv')
 config$date<-NULL
 
@@ -25,17 +26,17 @@ stopCluster(cl)
 hourlydat_sensor <- copy(rawdat)
 
 
-# EXTRACT COLUMNS ####
+# EXTRACT COLUMNS ----
 # don't need occupancy data, only volume data: 
 hourlydat_sensor[,c('occupancy.nulls', 'occupancy.sum', 'occupancy.mean', 'occupancy.median'):=NULL]
 
-# DELETE DUPLICATE OBSERVATIONS
+# DELETE DUPLICATE OBSERVATIONS ----
 # Find duplicate observations -- multiple pulls are written into the same data file
 # example  -- hourlydat_sensor[date == '2020-03-19' & sensor == 100]
 # get only the second observation:
 hourlydat_sensor <- hourlydat_sensor[!duplicated(hourlydat_sensor, by = c('date', 'hour', 'sensor'), fromLast=TRUE)]
 
-# EXPAND DATASET FOR MISSING DAYS OF DATA ####
+# EXPAND DATASET FOR MISSING DAYS OF DATA ----
 # for whole days with no observations, "hour" is NA. Get rid of these for now, then fill in
 hourlydat_sensor <- hourlydat_sensor[!is.na(hour)]
 
@@ -49,12 +50,12 @@ master_mat <- master_mat[,lapply(.SD, as.character), .SDcols = c('date', 'sensor
 # merge master to hourly data to fill in NA values for missing observations
 hourlydat_sensor <- merge(hourlydat_sensor, master_mat, by = c('date', 'sensor', 'hour'), all = T)
 
-# FORMAT DATE ####
+# FORMAT DATE ----
 # dealing with date
 hourlydat_sensor[,date:=as.IDate(fast_strptime(date, "%Y-%m-%d"))] 
 hourlydat_sensor[,year:=year(date)]
 
-# GAPFILLING 1: FILL IN MISSING MINUTES WITHIN HOURS ####
+# GAPFILLING 1: FILL IN MISSING MINUTES WITHIN HOURS ----
 # sometimes a few observations were missing within an hour (volume.nulls). 
 # each hour had 120 30-second observation windows.
 # calculate a new volume with the average volume for that hour, as long as there are at least 10 minutes of data (less than 100 nulls)
@@ -66,7 +67,7 @@ hourlydat_sensor[,volume.sum := ifelse(volume.nulls <=100, round(volume.sum.raw 
 hourlydat_sensor[,volume.sum := ifelse(volume.nulls>100, NA, volume.sum)]
 
 
-# GET RID OF IMPOSSIBLE VOLUME VALUES ####
+# GET RID OF IMPOSSIBLE VOLUME VALUES ----
         # hist(hourlydat_sensor$volume.sum)
         # hourlydat_sensor[volume.sum>3000] # greater than 3000 cars per hour? 
         # uniqueN(hourlydat_sensor[volume.sum>3000,sensor])  # 117 sensors
@@ -82,10 +83,10 @@ hourlydat_sensor <- hourlydat_sensor[,volume.sum:=ifelse(volume.sum>=3000, NA, v
 # 0 cars per hour? 
 # hist(hourlydat_sensor[volume.sum == 0, date], breaks = 'weeks')
 
-# DELETE OCCUPANCY COLUMNS - DON'T NEED ####
+# DELETE OCCUPANCY COLUMNS - DON'T NEED ----
 hourlydat_sensor[,c('volume.nulls', 'volume.mean', 'volume.median', 'volume.sum.raw'):=NULL]
 
-# GAPFILLING 2: WHOLE HOURS MISSING ####
+# GAPFILLING 2: WHOLE HOURS MISSING ----
 # if a whole hour is missing, fill in with average of the two hours on either side of it
 setorder(hourlydat_sensor, sensor, year, date, hour)
 hourlydat_sensor[,`:=`(volume.sum.rollmean = shift(frollapply(volume.sum, 3, mean, align = 'center', na.rm = T, hasNA = T))), 
@@ -100,18 +101,18 @@ hourlydat_sensor <- hourlydat_sensor[hour > 0]
 hourlydat_sensor[,c('volume.sum.rollmean'):=NULL]
 
 
-# AGGREGATE TO NODES ####
+# AGGREGATE TO NODES ----
 hourlydat_sensor <- merge(hourlydat_sensor, config, all.x = T, all.y = F, 
                    by.x = 'sensor', by.y = 'detector_name')
 
-# count number of lanes with data for each hour, at node
+# count number of lanes with data for each hour, at node ----
 hourlydat_sensor[,num_lanes_with_data_this_hr:=sum(!is.na(volume.sum)), 
           by = .(r_node_name, r_node_lanes, r_node_lon, r_node_lat, 
                  corridor_route, corridor_dir,
                  year, date, hour)]
 
-# hist(hourlydat_sensor[,num_lanes_with_data-r_node_lanes], 
-#      breaks = 2*max(abs(hourlydat_sensor[,num_lanes_with_data-r_node_lanes])))
+# hist(hourlydat_sensor[,num_lanes_with_data_this_hr-r_node_lanes], 
+#      breaks = 2*max(abs(hourlydat_sensor[,num_lanes_with_data_this_hr-r_node_lanes])))
 
 # sometimes more lanes than the node is reported to have from mndot (r_node_lanes). sometimes less. Confusing!
 # count number of unique sensors this year for this node: 
@@ -120,7 +121,7 @@ hourlydat_sensor[,num_sensors_this_year:=uniqueN(sensor),
 
 
 # hourlydat_sensor[num_sensors_this_year>6,]
-# hist(hourlydat_sensor$num_sensors_this_year - hourlydat_sensor$num_lanes_with_data)
+# hist(hourlydat_sensor$num_sensors_this_year - hourlydat_sensor$num_lanes_with_data_this_hr)
 # Yes, I believe this number of sensors instead
 
 # exclude to only those hourly observations at each node where the number of lanes
@@ -129,17 +130,17 @@ hourlydat_sensor <- hourlydat_sensor[num_lanes_with_data_this_hr == num_sensors_
 
 fwrite(hourlydat_sensor, 'data/data_hourly_bysensor_clean.csv')
 
-hourlydat_node <- hourlydat[,lapply(.SD, sum), 
+hourlydat_node <- hourlydat_sensor[,lapply(.SD, sum), 
                        .SDcols = 'volume.sum',
                        by = .(r_node_name, r_node_lon, r_node_lat, 
                               corridor_route, corridor_dir,
                               year, date, hour,
-                              num_lanes_with_data, num_sensors_this_year)]
+                              num_lanes_with_data_this_hr, num_sensors_this_year)]
 
 fwrite(hourlydat_node, 'data/data_hourly_bynode_clean.csv')
 
-# sum for all hours of the day (daily-scale data)
-dailydat <- hourlydat[,lapply(.SD, sum), .SDcols = 'volume.sum', 
+# sum for all hours of the day (daily-scale data) ----
+dailydat <- hourlydat_node[,lapply(.SD, sum), .SDcols = 'volume.sum', 
                       by = .(r_node_name, r_node_lon, r_node_lat, 
                              num_sensors_this_year,
                              corridor_route, corridor_dir,
