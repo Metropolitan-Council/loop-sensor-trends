93e0dcbd5f38974f53fb51b9ae07be3b906b6659
Ashley Asmus <ashley.asmus@gmail.com>
2020-03-25 11:28:32 -0500

    mapping nodes

--- 4-create-r-data-object-for-app.R
+++ 4-create-r-data-object-for-app.R
@@ -19,6 +19,10 @@ node_dt <- unique(sensor_dt[,.(r_node_name, r_node_n_type, r_node_lon, r_node_la
 
 #...shapefile ####
 node_sf <- st_as_sf(node_dt, coords = c('r_node_lon', 'r_node_lat'), crs = 4326)
+leaflet(node_sf)%>%
+  addCircleMarkers()%>%
+  addProviderTiles('CartoDB.Positron')
+
 # st_write(node_sf, 'node', driver = 'ESRI Shapefile')
 
 
