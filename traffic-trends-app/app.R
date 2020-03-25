3f142cd8fe0649030bc0abe450d5ac22d46b1b0d
Liz Roten <elizabetheroten@gmail.com>
2020-03-25 10:58:19 -0500

    rearrange; change color palette; add sidebar layout

--- traffic-trends-app/app.R
+++ traffic-trends-app/app.R
@@ -1,109 +1,121 @@
-#### Toolbox #### -----
-library(shiny)
-library(data.table)
-library(leaflet)
-library(sf)
-library(ggplot2)
-library(plotly)
-
-#### Load Data #### -----
-load("appdata.RData") # takes six seconds, not bad.
-options(shiny.launch.browser = TRUE,
-        scipen = 99999)
-
-binpal_ctu <- colorNumeric("RdYlBu", min(ctu_diffs_sf$avg.diff):max(ctu_diffs_sf$avg.diff), reverse = T)
-
+source("global.R")
 #### UI #### -----
 ui <- fluidPage(
     
+    tags$head(
+        tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+    ),
     titlePanel("Metro Area Vehicle Miles Traveled on MnDOT Roads"),
-    sidebarPanel(
-        h3("Select date"),
-        # Date selector
-        dateInput("selected_date", "Date:",
-                  value = "2020-03-23",
-                  min = min(diffs_dt$date), max = max(diffs_dt$date),
-                  format = "yyyy-mm-dd", startview = "month"
+    sidebarLayout(
+        sidebarPanel(width = 2,
+                     h3("Select date"),
+                     # Date selector
+                     dateInput("selected_date", "Date:",
+                               value = "2020-03-23",
+                               min = min(diffs_dt$date), max = max(diffs_dt$date),
+                               format = "yyyy-mm-dd", startview = "month"
+                     )
+                     
+                     
         ),
         
-        plotOutput("graph")
-        
-    ),
-    
-    
-    # Application title
-    
-    # Map and Plot
-    fluidRow(
-        column(5,
-            leafletOutput("mymap", height = "800px")
+        # Application title
+        mainPanel(
+            # Map and Plot
+            fluidRow(
+                column(10,
+                       leafletOutput("mymap", height = "600px")
+                )
+            ),
+            fluidRow( width = 10, 
+                      
+                plotOutput("graph")
+                
+            )
         )
     )
+        
+    )
     
-)
-
-#### Server #### -----
-server <- function(input, output) {
-    output$graph <- renderPlot({
-        ggplot(
-            diffs_4plot[doy > 61 & year == 2020],
-            aes(
-                x = date, y = `Difference from Typical VMT (%)`,
-                group = year,
-                text = paste0(
-                    "Date: ", date, " (", weekday, ")",
-                    "<br>Typical VMT: ", round(vmt.predict / 1000000, 1),
-                    " M (", round(vmt.predict / hh_total), " miles per household)",
-                    "<br>Actual VMT: ", round(vmt.sum / 1000000, 1),
-                    " M (", round(vmt.sum / hh_total), " miles per household)",
-                    "<br>Difference from Typical VMT: ", round(`Difference from Typical VMT (%)`), " %"
-                ),
-            )
-        ) +
-            theme_minimal() +
-            geom_point() +
-            geom_line() +
-            scale_x_date(date_breaks = "day", date_labels = "%b %d\n(%A)") +
-            geom_hline(yintercept = 0) +
-            ggtitle("Metro Area Traffic on MnDOT Roads") +
-            theme(legend.position = "none") +
-            labs(x = "Date", y = "% Difference from Typical VMT\n(Vehicle Miles Traveled)")
-    })
+    #### Server #### -----
+    server <- function(input, output) {
+        output$graph <- renderPlot({
+            ggplot(
+                diffs_4plot[doy > 61 & year == 2020],
+                aes(
+                    x = date, y = `Difference from Typical VMT (%)`,
+                    group = year,
+                    text = paste0(
+                        "Date: ", date, " (", weekday, ")",
+                        "<br>Typical VMT: ", round(vmt.predict / 1000000, 1),
+                        " M (", round(vmt.predict / hh_total), " miles per household)",
+                        "<br>Actual VMT: ", round(vmt.sum / 1000000, 1),
+                        " M (", round(vmt.sum / hh_total), " miles per household)",
+                        "<br>Difference from Typical VMT: ", round(`Difference from Typical VMT (%)`), " %"
+                    ),
+                )
+            ) +
+                theme_minimal() +
+                geom_point() +
+                geom_line() +
+                scale_x_date(date_breaks = "day", date_labels = "%b %d\n(%A)") +
+                geom_hline(yintercept = 0) +
+                ggtitle("Metro Area Traffic on MnDOT Roads") +
+                theme(legend.position = "none") +
+                labs(x = "Date", y = "% Difference from Typical VMT\n(Vehicle Miles Traveled)")
+        })
+        
+        # base map -------------------------------------------------------------------------------------------
+        output$mymap <- renderLeaflet({
+            leaflet(ctu_diffs_sf) %>%
+                addProviderTiles("CartoDB.Positron") %>%
+                addMapPane("polygons", zIndex = 410) %>%
+                addMapPane("points", zIndex = 420) %>%
+                addCircleMarkers(
+                    data = node_sf, color = "white", radius = 2, fill = FALSE,
+                    group = "Traffic Detector Locations",
+                    options = leafletOptions(pane = "points")
+                )
+            # addPolygons(data = ctu_diffs_sf[ctu_diffs_sf$date == '2020-03-23' ,],
+            #             color = ~binpal(avg.diff), stroke = T, fillOpacity = 0.75,
+            #             label = ctu_labels[ctu_diffs_dt$date == '2020-03-23'])
+        })
+        
+        binpal_ctu <- colorBin(palette = "PuOr", 
+                               domain = ctu_diffs_dt$avg.diff,
+                               bins = 5,
+                               reverse = T)
+        
+        # when I select a date------------------------------------------------------
+        observeEvent(input$selected_date, {
+            new_ctu_diffs <- ctu_diffs_sf[as.character(ctu_diffs_sf$date) == input$selected_date, ]
+            new_ctu_labels <- ctu_labels[as.character(ctu_diffs_dt$date) == input$selected_date]
+            # new map - this date
+            leafletProxy("mymap") %>%
+                clearGroup("ctu_dat") %>%
+                clearControls() %>% 
+                addPolygons(
+                    data = new_ctu_diffs,
+                    fillColor = ~binpal_ctu(avg.diff),
+                    color = "lightgray",
+                    stroke = T, 
+                    weight = 1,
+                    fillOpacity = 0.85,
+                    label = new_ctu_labels,
+                    group = "ctu_dat",
+                    options = leafletOptions(pane = "polygons"),
+                    labelOptions = labelOptions(textsize = "1.6rem")
+                ) %>% 
+                addLegend(data = new_ctu_diffs,
+                          position = "bottomright",
+                          group = "legend",
+                          pal = binpal_ctu,
+                          title = "Avg. diff",
+                          values = ~avg.diff,
+                          labFormat = labelFormat(suffix = "%"))
+        }) # end household marker click routine
+    }
     
-    # base map -------------------------------------------------------------------------------------------
-    output$mymap <- renderLeaflet({
-        leaflet(ctu_diffs_sf) %>%
-            addProviderTiles("CartoDB.Positron") %>%
-            addMapPane("polygons", zIndex = 410) %>%
-            addMapPane("points", zIndex = 420) %>%
-            addCircleMarkers(
-                data = node_sf, color = "white", radius = 2, fill = FALSE,
-                group = "Traffic Detector Locations",
-                options = leafletOptions(pane = "points")
-            )
-        # addPolygons(data = ctu_diffs_sf[ctu_diffs_sf$date == '2020-03-23' ,],
-        #             color = ~binpal(avg.diff), stroke = T, fillOpacity = 0.75,
-        #             label = ctu_labels[ctu_diffs_dt$date == '2020-03-23'])
-    })
+    # Run the application
+    shinyApp(ui = ui, server = server)
     
-    # when I select a date------------------------------------------------------
-    observeEvent(input$selected_date, {
-        new_ctu_diffs <- ctu_diffs_sf[as.character(ctu_diffs_sf$date) == input$selected_date, ]
-        new_ctu_labels <- ctu_labels[as.character(ctu_diffs_dt$date) == input$selected_date]
-        # new map - this date
-        leafletProxy("mymap") %>%
-            clearGroup("ctu_dat") %>%
-            addPolygons(
-                data = new_ctu_diffs,
-                color = ~ binpal(avg.diff),
-                stroke = T, 
-                fillOpacity = 0.75,
-                label = new_ctu_labels,
-                group = "ctu_dat",
-                options = leafletOptions(pane = "polygons")
-            )
-    }) # end household marker click routine
-}
-
-# Run the application
-shinyApp(ui = ui, server = server)
