d204536b2ee05141b44b2e719aaf193fd37ffe1e
Liz Roten <elizabetheroten@gmail.com>
2020-03-26 17:04:58 -0500

    add covid events dataset
    could display positive test result count, deaths, hospitalizations, etc

---
+++ covid.traffic.trends/data-raw/covid_events.R
@@ -0,0 +1,12 @@
+## code to prepare `covid_events` dataset goes here
+covid_events <- cbind(
+  date = c("2020-03-19",
+           "2020-03-21"),
+  death_count = c(1,
+                  1)
+  ) %>% 
+  data.table::as.data.table()
+  
+
+
+usethis::use_data(covid_events, overwrite = TRUE, compress = "xz")
