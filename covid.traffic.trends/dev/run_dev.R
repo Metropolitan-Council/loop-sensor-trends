b59053c40c672ff01cb67b5ab31857c15ad9e840
Liz Roten <elizabetheroten@gmail.com>
2020-03-26 11:47:44 -0500

    start about document

--- covid.traffic.trends/dev/run_dev.R
+++ covid.traffic.trends/dev/run_dev.R
@@ -4,8 +4,16 @@ options(shiny.launch.browser = TRUE,
         golem.app.prod = FALSE) # TRUE = production mode, FALSE = development mode
 
 # Detach all loaded packages and clean your environment
-golem::detach_all_attached()
+# golem::detach_all_attached()
 # rm(list=ls(all.names = TRUE))
+
+rmarkdown::render("./ABOUT.Rmd", output_format = "github_document",
+                  output_file = "ABOUT.md", output_dir = "./inst/app/www",
+                  params = list(actor_id = "roten",
+                                data_date = "2020-03-26",
+                                sha = system("git rev-parse --short HEAD",
+                                             intern=TRUE)))
+
 
 # Document and reload your package
 golem::document_and_reload()
---
+++ covid.traffic.trends/inst/app/www/ABOUT.md
@@ -0,0 +1,15 @@
+
+## About
+
+### Models
+
+### Contributors
+
+  - Liz Roten  
+  - Ashley Asmus  
+  - Jonathan Erhlich
+
+<right style="font-size: 1rem; text-align: right; display: block;">
+*Last updated 2020-03-26*  
+Build ID: 2020-03-26.roten.deede23  
+</right>
