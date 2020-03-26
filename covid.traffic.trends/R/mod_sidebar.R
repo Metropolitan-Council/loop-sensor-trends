6189ea72261c208aae2a766189cf4da930846f6d
Liz Roten <elizabetheroten@gmail.com>
2020-03-26 17:53:21 -0500

    add date input and reactive value

--- covid.traffic.trends/R/mod_sidebar.R
+++ covid.traffic.trends/R/mod_sidebar.R
@@ -10,23 +10,37 @@
 mod_sidebar_ui <- function(id){
   ns <- NS(id)
   tagList(
-
-    wellPanel(shinipsum::random_text(nwords = 30))
+    
+    wellPanel(
+      dateInput(ns("select_date"), "Date:",
+                        value = "2020-03-23",
+                        min = min(predicted_actual_by_region$date),
+                        max = max(predicted_actual_by_region$date),
+                        format = "yyyy-mm-dd", startview = "month"
+    ),
+    
+    shinipsum::random_text(nwords = 30))
     
   )
 }
-    
+
 #' sidebar Server Function
 #'
 #' @noRd 
 mod_sidebar_server <- function(input, output, session){
   ns <- session$ns
- 
+  vals <- reactiveValues()
+  
+  observeEvent(input$select_date, {
+    vals$date <- input$select_date
+  })
+  
+  return(vals)
 }
-    
+
 ## To be copied in the UI
 # mod_sidebar_ui("sidebar_ui_1")
-    
+
 ## To be copied in the server
 # callModule(mod_sidebar_server, "sidebar_ui_1")
- 
+
