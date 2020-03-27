17ea78bbd053879bb922238ba233bf69b4d604e4
Liz Roten <elizabetheroten@gmail.com>
2020-03-27 12:53:11 -0500

    rearrange plot and map

--- covid.traffic.trends/R/app_ui.R
+++ covid.traffic.trends/R/app_ui.R
@@ -24,14 +24,17 @@ app_ui <- function(request) {
                          skeleton::sk_col("sk_sidebar", width = 2,
                                           mod_sidebar_ui("sidebar_ui")
                          ),
+                         
                          skeleton::sk_col("sk_map", width = 10,
-                                          h5("Decreases in travel are occuring across the Twin Cities metropolitan region"),
-                                          
-                                          mod_leaflet_ui("leaflet_ui"),
+                                          ## plot
                                           h5("Travel decreased steadily in the days following the first COVID-19 case in Minnesota"),
-                                          mod_plot_ui("plot_ui_1")
+                                          mod_plot_ui("plot_ui_1"),
                                           
+                                          ## map
+                                          h5("Decreases in travel are occuring across the Twin Cities metropolitan region"),
+                                          mod_leaflet_ui("leaflet_ui")
                          )
+                         
         ),
         skeleton::sk_row(id = "about",
                          mod_about_ui("about_ui")
