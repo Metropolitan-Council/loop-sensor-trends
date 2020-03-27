7b8a3c93a951a6b27a08e8e206c2e927dedcc488
Liz Roten <elizabetheroten@gmail.com>
2020-03-26 19:34:35 -0500

    change input label; change headers

--- covid.traffic.trends/R/app_ui.R
+++ covid.traffic.trends/R/app_ui.R
@@ -12,8 +12,8 @@ app_ui <- function(request) {
     # List the first level UI elements here 
     fluidPage(
       skeleton::sk_page(
-        skeleton::sk_header("Minnesota COVID-19 Traffic Trends",
-                            shiny::h4("Are mitigation efforts affecting travel behavior?")),
+        skeleton::sk_header("Minnesotans Reduce Travel in Response to COVID-19",
+                            shiny::h4("Traffic Data Reveals More Minnesotans are Staying at Home During the COVID-19 Outbreak")),
         skeleton::sk_nav(
           skeleton::sk_nav_item("map_plot", "Map & Plot"),
           skeleton::sk_nav_item("about", "About")
@@ -23,10 +23,12 @@ app_ui <- function(request) {
                          width = 12,
                          skeleton::sk_col("sk_sidebar", width = 2,
                                           mod_sidebar_ui("sidebar_ui")
-                                          ),
+                         ),
                          skeleton::sk_col("sk_map", width = 10,
+                                          h5("Decreases in travel are occuring across the Twin Cities metropolitan region"),
+                                          
                                           mod_leaflet_ui("leaflet_ui"),
-                                          h5("Traffic Reduction"),
+                                          h5("Travel decreased steadily in the days following the first COVID-19 case in Minnesota"),
                                           mod_plot_ui("plot_ui_1")
                                           
                          )
@@ -46,7 +48,11 @@ app_ui <- function(request) {
           #----
           tags$a(href="https://metrocouncil.org", target="_blank",
                  img(src = "www/logo.png", align = "right", style = "padding: 1%"))
-        )    
+          # tags$div(
+          #   tags$a(href="https://github.com/Metropolitan-Council/loop-sensor-trends", target="_blank",
+          #          icon(name = "github", lib = "font-awesome"))
+          # )    
+        )
         
       )
     )
--- covid.traffic.trends/R/mod_sidebar.R
+++ covid.traffic.trends/R/mod_sidebar.R
@@ -12,14 +12,13 @@ mod_sidebar_ui <- function(id){
   tagList(
     
     wellPanel(
-      dateInput(ns("select_date"), "Date:",
-                        value = "2020-03-25",
-                        min = min(predicted_actual_by_region$date),
-                        max = max(predicted_actual_by_region$date),
-                        format = "yyyy-mm-dd", startview = "month"
-    ),
-    
-    shinipsum::random_text(nwords = 30))
+      dateInput(ns("select_date"), "Select a date to see change over time",
+                value = "2020-03-25",
+                min = min(predicted_actual_by_region$date),
+                max = max(predicted_actual_by_region$date),
+                format = "yyyy-mm-dd", startview = "month"
+      )
+    )
     
   )
 }
