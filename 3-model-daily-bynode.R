c0ceddde10e3abffac7f230753ae6f2b3552309f
Liz Roten <elizabetheroten@gmail.com>
2020-03-27 10:00:18 -0500

    use style linter; save data to output and covid.traffic.trends/data-raw

--- 3-model-daily-bynode.R
+++ 3-model-daily-bynode.R
@@ -6,23 +6,23 @@ library(mgcv)
 library(lubridate)
 #############
 
-dailydat <- fread('data/data_daily_bynode_clean.csv')
+dailydat <- fread("data/data_daily_bynode_clean.csv")
 
 # Dealing with date ----
-dailydat[,date:=as.IDate(date)]
-dailydat[,dow:=wday(date)]
-dailydat[,doy:=yday(date)]
-dailydat[,year:=year(date)]
-dailydat[,woy:=week(date)]
-dailydat[,weekday:=factor(weekdays(date))]
-dailydat[,monthday:=format(date, "%b %d")]
+dailydat[, date := as.IDate(date)]
+dailydat[, dow := wday(date)]
+dailydat[, doy := yday(date)]
+dailydat[, year := year(date)]
+dailydat[, woy := week(date)]
+dailydat[, weekday := factor(weekdays(date))]
+dailydat[, monthday := format(date, "%b %d")]
 
 
 # get rid of 2017 data: (december 15-31 included in this pull) ----
-dailydat <- dailydat[year>2017,]
+dailydat <- dailydat[year > 2017, ]
 
 # get rid of december data: ----
-dailydat <- dailydat[doy<100,]
+dailydat <- dailydat[doy < 100, ]
 
 dim(dailydat) # 348203     14
 
@@ -30,22 +30,22 @@ dim(dailydat) # 348203     14
 # ggplot(dailydat, aes(x = volume.sum, fill = factor(year)))+
 #   geom_density(alpha = 0.5)
 # some very high numbers
-dailydat[volume.sum>num_sensors_this_year * 23 * 2000] # reasonable though
+dailydat[volume.sum > num_sensors_this_year * 23 * 2000] # reasonable though
 
 # no such thing as 0 daily volume
-dailydat <- dailydat[volume.sum>0]
+dailydat <- dailydat[volume.sum > 0]
 
 # must have 3 years of data, at least 60 days of data in each year ----
-dailydat[,'num_days_per_year':=uniqueN(date), by = .(r_node_name, year)]
-dailydat <- dailydat[num_days_per_year>60]
+dailydat[, "num_days_per_year" := uniqueN(date), by = .(r_node_name, year)]
+dailydat <- dailydat[num_days_per_year > 60]
 
 has_2020_data <- unique(dailydat$r_node_name[dailydat$year == 2020])
 has_2018_data <- unique(dailydat$r_node_name[dailydat$year == 2018])
 has_2019_data <- unique(dailydat$r_node_name[dailydat$year == 2019])
 
 dailydat <- dailydat[dailydat$r_node_name %in% has_2020_data
-                     & dailydat$r_node_name %in% has_2019_data
-                     & dailydat$r_node_name %in% has_2018_data,]
+& dailydat$r_node_name %in% has_2019_data
+& dailydat$r_node_name %in% has_2018_data, ]
 dim(dailydat) # 322241     15
 
 dailydat_s <- split(dailydat, dailydat$r_node_name)
@@ -56,48 +56,51 @@ gam_list <- vector("list", length(dailydat_s))
 
 
 # MODEL TIME ----
-for(s in seq_along(dailydat_s)){
+for (s in seq_along(dailydat_s)) {
   # print(s)
   # flush.console()
   this_dat <- dailydat_s[[s]]
-  # subset to relevant dates: 
-  modeling_dat <- this_dat[this_dat$date < '2020-03-01' 
-                           & this_dat$doy <= 90 # feed it relevant dates - before april 1
-                           & this_dat$doy >1,] # exclude the one major holiday in here - jan 1
-  
-  modeling_dat <- modeling_dat[!modeling_dat$date == '2020-01-17',] # cold snap - exclude
-  modeling_dat <- modeling_dat[!modeling_dat$date == '2020-01-18',] # cold snap - exclude
-  modeling_dat <- modeling_dat[!modeling_dat$date == '2020-02-09',] # snow day - exclude
-  
-  this_gam <- with(modeling_dat,
-                   mgcv::gam(volume.sum ~ 
-                               s(dow, k = 7, by = as.factor(year)) # one knot for each day of the week
-                             + s(doy) #general seasonal trend, let it vary by year, allow knots to be set by gam
-                             + as.factor(year) # intercept for each year
-                   ))
-  
+  # subset to relevant dates:
+  modeling_dat <- this_dat[this_dat$date < "2020-03-01"
+  & this_dat$doy <= 90 # feed it relevant dates - before april 1
+  & this_dat$doy > 1, ] # exclude the one major holiday in here - jan 1
+
+  modeling_dat <- modeling_dat[!modeling_dat$date == "2020-01-17", ] # cold snap - exclude
+  modeling_dat <- modeling_dat[!modeling_dat$date == "2020-01-18", ] # cold snap - exclude
+  modeling_dat <- modeling_dat[!modeling_dat$date == "2020-02-09", ] # snow day - exclude
+
+  this_gam <- with(
+    modeling_dat,
+    mgcv::gam(
+      volume.sum ~
+      s(dow, k = 7, by = as.factor(year)) # one knot for each day of the week
+      + s(doy) # general seasonal trend, let it vary by year, allow knots to be set by gam
+        + as.factor(year) # intercept for each year
+    )
+  )
+
   gam_list[[s]] <- this_gam
-  
-  this_dat[,c('volume.predict', 'volume.predict.se'):=cbind(predict.gam(object = this_gam, newdata = this_dat, se.fit = T))]
-  
+
+  this_dat[, c("volume.predict", "volume.predict.se") := cbind(predict.gam(object = this_gam, newdata = this_dat, se.fit = T))]
+
   # difference from predicted, n volume:
-  this_dat[,volume.diff.raw:=(volume.sum - volume.predict)]
-  
-  # difference from predicted, in %: 
-  this_dat[,volume.diff := round( ( (volume.sum - volume.predict) / volume.predict) * 100, 1)]
-  
+  this_dat[, volume.diff.raw := (volume.sum - volume.predict)]
+
+  # difference from predicted, in %:
+  this_dat[, volume.diff := round(((volume.sum - volume.predict) / volume.predict) * 100, 1)]
+
   # store difference from normal for 2020 for mapping
   this_diff <- this_dat
-  diffs_ls[[s]]<-this_diff
+  diffs_ls[[s]] <- this_diff
 }
 
 diffs_dt <- rbindlist(diffs_ls)
 # saveRDS(gam_list, file = paste0('data/gam-models-', Sys.Date(), '.RData'))
 
-# some very high numbers? 
+# some very high numbers?
 # hist(diffs_dt[,volume.diff])
 # summary(diffs_dt[,volume.diff])
-unique(diffs_dt[volume.diff < (-100),.(r_node_name, r_node_n_type)])
+unique(diffs_dt[volume.diff < (-100), .(r_node_name, r_node_n_type)])
 # r_node_name r_node_n_type
 # 1:    rnd_1576          Exit
 # 2:    rnd_5932          Exit
@@ -119,10 +122,10 @@ unique(diffs_dt[volume.diff < (-100),.(r_node_name, r_node_n_type)])
 # 18:   rnd_89289      Entrance
 # 19:   rnd_91110          Exit
 # 20:   rnd_95341          Exit
-unique(diffs_dt[volume.predict<0, .(r_node_name, r_node_n_type)])
-# negative predicted values -- one spurious node 
+unique(diffs_dt[volume.predict < 0, .(r_node_name, r_node_n_type)])
+# negative predicted values -- one spurious node
 # diffs_dt <- diffs_dt[!r_node_name == 'rnd_86223'] -- now many more
-diffs_dt<-diffs_dt[!r_node_name %in% unique(diffs_dt[volume.predict<0, r_node_name])]
+diffs_dt <- diffs_dt[!r_node_name %in% unique(diffs_dt[volume.predict < 0, r_node_name])]
 
 # other spurious observations (8 of them)
 # diffs_dt<-diffs_dt[volume.diff < (100)]
@@ -130,9 +133,9 @@ diffs_dt<-diffs_dt[!r_node_name %in% unique(diffs_dt[volume.predict<0, r_node_na
 # diffs_dt <- diffs_dt[!r_node_name %in% c('rnd_86469', 'rnd_95784')]
 
 # very high values
-summary(diffs_dt[volume.diff>100 & year == 2020,.(volume.predict, volume.diff)])
+summary(diffs_dt[volume.diff > 100 & year == 2020, .(volume.predict, volume.diff)])
 # how many of these?
-unique(diffs_dt[volume.diff>500 & year == 2020, .(r_node_name, r_node_n_type, date)])
+unique(diffs_dt[volume.diff > 500 & year == 2020, .(r_node_name, r_node_n_type, date)])
 # r_node_name r_node_n_type       date
 # 1:     rnd_849          Exit 2020-03-15
 # 2:     rnd_849          Exit 2020-03-16
@@ -149,12 +152,13 @@ unique(diffs_dt[volume.diff>500 & year == 2020, .(r_node_name, r_node_n_type, da
 # 13:   rnd_89451      Entrance 2020-03-22
 
 # get rid of thse as well
-diffs_dt<-diffs_dt[!r_node_name %in% unique(diffs_dt[volume.diff>500 & year == 2020, r_node_name])]
-diffs_dt[,date:=as.IDate(date)]
+diffs_dt <- diffs_dt[!r_node_name %in% unique(diffs_dt[volume.diff > 500 & year == 2020, r_node_name])]
+diffs_dt[, date := as.IDate(date)]
 # Trim to after March 1 2020:
-diffs_dt <- diffs_dt[date>='2020-03-01',]
+diffs_dt <- diffs_dt[date >= "2020-03-01", ]
 
-fwrite(diffs_dt, paste0('output/pred-and-act-vol-by-node-', Sys.Date(), '.csv'))
+fwrite(diffs_dt, paste0("output/pred-and-act-vol-by-node-", Sys.Date(), ".csv"))
+fwrite(diffs_dt, paste0("covid.traffic.trends/data-raw/pred-and-act-vol-by-node-", Sys.Date(), ".csv"))
 
 
 # More data reshaping of model output ----
@@ -162,30 +166,18 @@ fwrite(diffs_dt, paste0('output/pred-and-act-vol-by-node-', Sys.Date(), '.csv'))
 
 # Total difference from expected for whole metro area ----
 diffs_4plot <- diffs_dt[r_node_n_type == "Station" & year == 2020 # this year's data, stations only. ####
-                        ,lapply(.SD, FUN = function(x) sum(x, na.rm = T)),
-                       .SDcols = c('volume.sum', 'volume.predict'), 
-                       by = .(date, dow, doy, year, woy, weekday, monthday)]
+  , lapply(.SD, FUN = function(x) sum(x, na.rm = T)),
+  .SDcols = c("volume.sum", "volume.predict"),
+  by = .(date, dow, doy, year, woy, weekday, monthday)
+]
 
 # vmt is 1/2 of volume ----
-diffs_4plot[,c("vmt.sum", "vmt.predict"):=list(volume.sum*0.5, volume.predict * 0.5)]
-diffs_4plot[,'Difference from Typical VMT (%)':=round(100*(vmt.sum-vmt.predict)/vmt.predict, 2)]
-diffs_4plot[,difference_text:=ifelse(`Difference from Typical VMT (%)` <0, paste0(abs(round(`Difference from Typical VMT (%)`, 1)), " % less than typical"),
-                                                 paste0(abs(round(`Difference from Typical VMT (%)`, 1)), " % more than typical"))]
+diffs_4plot[, c("vmt.sum", "vmt.predict") := list(volume.sum * 0.5, volume.predict * 0.5)]
+diffs_4plot[, "Difference from Typical VMT (%)" := round(100 * (vmt.sum - vmt.predict) / vmt.predict, 2)]
+diffs_4plot[, difference_text := ifelse(`Difference from Typical VMT (%)` < 0, paste0(abs(round(`Difference from Typical VMT (%)`, 1)), " % less than typical"),
+  paste0(abs(round(`Difference from Typical VMT (%)`, 1)), " % more than typical")
+)]
 
 
-fwrite(diffs_4plot, paste0('output/pred-and-act-vol-region-', Sys.Date(), '.csv'))
-
-# # melt to long form for plotting predicted and acutals simultaneoulsy in plotly ----
-# nahhhh this isn't useful really
-
-# diffs_4plot_long <- melt(diffs_4plot[,.(vmt.sum, vmt.predict, date, dow, doy, year, woy, weekday, monthday, `Difference from Typical VMT (%)`)], 
-#                         id.vars = c('date', 'dow', 'doy', 'year', 'woy', 'weekday', 'monthday', "Difference from Typical VMT (%)"),
-#                         variable.name = "estimate_type", value.name = "VMT")
-# diffs_4plot_long$estimate_type <- ifelse(diffs_4plot_long$estimate_type == "vmt.sum", "Actual Traffic", "Typical Traffic")
-# 
-# diffs_4plot_long[,difference_text:=ifelse(estimate_type == "Actual Traffic", 
-#                                          ifelse(`Difference from Typical VMT (%)` <0, paste0(abs(`Difference from Typical VMT (%)`), " % less than typical"),
-#                                                 paste0(abs(round(`Difference from Typical VMT (%)`, 1)), " % more than typical")),
-#                                          ifelse(`Difference from Typical VMT (%)` <0, paste0(abs(`Difference from Typical VMT (%)`), " % more than actual"),
-#                                                 paste0(abs(round(`Difference from Typical VMT (%)`, 1)), " % less than actual")))]
-# fwrite(diffs_4plot_long, paste0('data/pred-and-act-vol-for-plotting-long-', Sys.Date(), '.csv'))
+fwrite(diffs_4plot, paste0("output/pred-and-act-vol-region-", Sys.Date(), ".csv"))
+fwrite(diffs_4plot, paste0("covid.traffic.trends/data-raw/pred-and-act-vol-region-", Sys.Date(), ".csv"))
