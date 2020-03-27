b66ab3f809bc3585a93157e7c0a94eed217db185
Liz Roten <elizabetheroten@gmail.com>
2020-03-27 10:13:49 -0500

    add syntax for getting daily state updates

--- covid.traffic.trends/data-raw/predicted_actual_traffic.R
+++ covid.traffic.trends/data-raw/predicted_actual_traffic.R
@@ -11,7 +11,10 @@ predicted_actual_by_node[,date:=as.IDate(date)]
 predicted_actual_by_node <- predicted_actual_by_node[date>'2020-03-01',]
 predicted_actual_by_node <- predicted_actual_by_node[,scl_volume:=scale(volume.predict, center= F)] %>% 
   mutate(hover_text = paste(sep = "", "<b>", format.Date(date, "%B %d"), "</b>",  "<br>",
-                            volume.diff, "%"))
+                            volume.diff, "%"),
+         District = "MnDOT Metro Freeways"
+         
+  )
 
 
 usethis::use_data(predicted_actual_by_node, overwrite = TRUE, compress = "xz")
@@ -30,7 +33,52 @@ predicted_actual_by_region <- fread(paste0('./data-raw/pred-and-act-vol-region-'
   mutate(typical_vmt_diff = `Difference from Typical VMT (%)`) %>% 
   select(-`Difference from Typical VMT (%)`) %>% 
   mutate(hover_text = paste(sep = "", "<b>", format.Date(date, "%B %d"), "</b>",  "<br>",
-                            typical_vmt_diff, "%")
+                            typical_vmt_diff, "%"),
+         District = "MnDOT Metro Freeways"
   )
 
 usethis::use_data(predicted_actual_by_region, overwrite = TRUE, compress = "xz")
+
+
+# MNDOT Traffic Trends -----
+
+
+
+get_mndot_data <- function() {
+  
+  yesterday <- Sys.Date() - 1
+  yesterday <- as.IDate(yesterday)
+  yesterday <- paste0(month(yesterday), "-", mday(yesterday), "-", year(yesterday))
+  
+  day_before_yesterday <- Sys.Date() - 2
+  day_before_yesterday <- as.IDate(day_before_yesterday)
+  day_before_yesterday <- paste0(month(day_before_yesterday), "-", mday(day_before_yesterday), "-", year(day_before_yesterday))
+  
+  try_today <- try(suppressWarnings(fread(paste0("http://www.dot.state.mn.us/traffic/data/reports/COVD19/Daily_Volume_Change_", yesterday, "_update.csv"),
+                         verbose = FALSE,
+                         showProgress = FALSE)
+                         ),
+                   silent = TRUE
+  )
+  if(class(try_today) == "try-error") {
+    message("Data for ", yesterday, " is unavailable")
+    message("Returning data for ", day_before_yesterday)
+    final <- fread(paste0("http://www.dot.state.mn.us/traffic/data/reports/COVD19/Daily_Volume_Change_",
+                          day_before_yesterday, "_update.csv"),
+                   verbose = FALSE,
+                   showProgress = FALSE)
+  } else {
+    final <- try_today
+  }
+  return(final)  
+}
+
+predicted_actual_by_state <- get_mndot_data()
+
+predicted_actual_by_state <- predicted_actual_by_state[District %in% c("MnDOT Statewide")]
+predicted_actual_by_state <- melt(predicted_actual_by_state, id.vars = c("District"),
+                                  variable.name = "date", 
+                                  value.name = "typical_vmt_diff")
+predicted_actual_by_state <- predicted_actual_by_state[, date := as.IDate(date, format = "%m/%d/%Y")]
+
+usethis::use_data(predicted_actual_by_state, overwrite = TRUE, compress = "xz")
