a647b00108f67f4fdb219cfeece0ec9ed25971b6
Liz Roten <liz.roten@metc.state.mn.us>
2020-03-28 14:06:22 -0500

    update mn_actions and state predicted data

--- covid.traffic.trends/data-raw/covid_events.R
+++ covid.traffic.trends/data-raw/covid_events.R
@@ -1,4 +1,8 @@
 ## code to prepare `covid_events` dataset goes here
+
+library(dplyr)
+library(data.table)
+
 covid_events <- cbind(
   date = c("2020-03-19",
            "2020-03-21"),
--- covid.traffic.trends/data-raw/mn_actions.R
+++ covid.traffic.trends/data-raw/mn_actions.R
@@ -6,22 +6,22 @@ mn_actions <- cbind(
            '2020-03-11', # UMN Suspends In-Person Classes
            '2020-03-13', # Gov. Walz declares peacetime emergency; calls for cancellation of events >250 ppl
            '2020-03-15', # Gov. Walz announces public schools will close by Mar. 18
-           '2020-03-18' # Gov. Walz & MDH ask all gyms, bars, public spaces to close, restaurants limit to take-out
-           # '2020-03-27' # Gov. Walz & MDH ask everyone to stay home except for essential needs
+           '2020-03-18', # Gov. Walz & MDH ask all gyms, bars, public spaces to close, restaurants limit to take-out
+           '2020-03-27' # Gov. Walz & MDH ask everyone to stay home except for essential needs
            ),
   action = c('MN Dept. of Health confirms 1st COVID-19 case in MN', 
              'UMN Suspends In-Person Classes', 
              'Gov. Walz declares peacetime emergency', 
              'Gov. Walz & MDH announce public schools will close by March 18',
-             'Gov. Walz & MDH ask all gyms, bars, public spaces to close, restaurants limit to take-out'
-             # 'Gov. Walz & MDH ask everyone to stay home except for essential needs'
+             'Gov. Walz & MDH ask all gyms, bars, public spaces to close, restaurants limit to take-out',
+             'Gov. Walz & MDH ask everyone to stay home except for essential needs'
   ),
   link = c('https://www.health.state.mn.us/diseases/coronavirus/situation.html',
            'https://safe-campus.umn.edu/public-health-alerts',
            'https://mn.gov/governor/assets/EO%2020-01_tcm1055-422957.pdf',
            'https://mn.gov/governor/news/?id=1055-423071',
-           'https://mn.gov/governor/assets/2020_03_16_EO_20_04_Bars_Restaurants_tcm1055-423380.pdf'
-           # 'https://mn.gov/governor/covid-19/'
+           'https://mn.gov/governor/assets/2020_03_16_EO_20_04_Bars_Restaurants_tcm1055-423380.pdf',
+           'https://mn.gov/governor/covid-19/'
            )
 ) %>% 
   data.table::as.data.table() %>% 
