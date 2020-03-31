199d0399cee124b986284a0a5d26fe7c518ba778
Liz Roten <liz.roten@metc.state.mn.us>
2020-03-31 16:07:54 -0500

    break out sidebars into two distinct UI modules

--- covid.traffic.trends/R/mod_sidebar.R
+++ covid.traffic.trends/R/mod_sidebar.R
@@ -7,35 +7,19 @@
 #' @noRd 
 #'
 #' @importFrom shiny NS tagList 
-mod_sidebar_ui <- function(id){
+mod_sidebar_ui <- function(id, pair){
   ns <- NS(id)
   tagList(
-    
-    wellPanel(id = "sidebar_panel",
-              
-              p("This plot shows the daily relative decrease in freeway travel over time across the Twin Cities metropolitan region after March 1. Points that fall below the zero-line represent decreases in travel relative to typical travel on that day of the year and day of the week. Typical travel is estimated using a statistical analysis of traffic volumes from 2018, 2019, and 2020 prior to March 1. "),
-              br(),
-              br(),
-              br(),
-              br(),
-              br(),
-              br(),
-              br(),
-              br(),
-              br(),
-              br(),
-              br(),
-              p("The map shows the decreases in travel at individual traffic monitoring sites across the Twin Cities Metropolitan area. Traffic monitoring is performed by the Minnesota Department of Transportation (MnDOT) using detectors built into the infrastructure of the roads. These detectors are usually used to estimate congestion along Metro area highways. "),
-              
-      dateInput(ns("select_date"), "Select a date to see change in the map over time",
-                value = max(covid.traffic.trends::predicted_actual_by_region$date),
-                min = min(covid.traffic.trends::predicted_actual_by_region$date),
-                max = max(covid.traffic.trends::predicted_actual_by_region$date),
-                format = "mm/dd/yyyy",
-                startview = "month"
+    if(pair == "map"){
+      wellPanel(
+        p("The map shows the decreases in travel at individual traffic monitoring sites across the Twin Cities Metropolitan area. Traffic monitoring is performed by the Minnesota Department of Transportation (MnDOT) using detectors built into the infrastructure of the roads. These detectors are usually used to estimate congestion along Metro area highways. "),
+        mod_map_inputs_ui("map_inputs_ui_1")
       )
-    )
-    
+    } else {
+      wellPanel(
+        p("This plot shows the daily relative decrease in freeway travel over time across the Twin Cities metropolitan region after March 1. Points that fall below the zero-line represent decreases in travel relative to typical travel on that day of the year and day of the week. Typical travel is estimated using a statistical analysis of traffic volumes from 2018, 2019, and 2020 prior to March 1. ")
+      )
+    }
   )
 }
 
@@ -44,13 +28,7 @@ mod_sidebar_ui <- function(id){
 #' @noRd 
 mod_sidebar_server <- function(input, output, session){
   ns <- session$ns
-  vals <- reactiveValues()
   
-  observeEvent(input$select_date, {
-    vals$date <- input$select_date
-  })
-  
-  return(vals)
 }
 
 ## To be copied in the UI
