3daa1d53b9aeb43adf4e53b382175792da760b95
Liz Roten <liz.roten@metc.state.mn.us>
2020-03-31 16:09:31 -0500

    make a new map input module

---
+++ covid.traffic.trends/R/mod_map_inputs.R
@@ -0,0 +1,44 @@
+#' map_inputs UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd 
+#'
+#' @importFrom shiny NS tagList 
+mod_map_inputs_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+    dateInput(ns("select_date"), "Select a date to see change in the map over time",
+              value = max(covid.traffic.trends::predicted_actual_by_region$date),
+              min = min(covid.traffic.trends::predicted_actual_by_region$date),
+              max = max(covid.traffic.trends::predicted_actual_by_region$date),
+              format = "mm/dd/yyyy",
+              startview = "month"
+    )
+    
+ 
+  )
+}
+    
+#' map_inputs Server Function
+#'
+#' @noRd 
+mod_map_inputs_server <- function(input, output, session){
+  ns <- session$ns
+  vals <- reactiveValues()
+  
+  observeEvent(input$select_date, {
+    vals$date <- input$select_date
+  })
+  
+  return(vals)
+}
+    
+## To be copied in the UI
+# mod_map_inputs_ui("map_inputs_ui_1")
+    
+## To be copied in the server
+# callModule(mod_map_inputs_server, "map_inputs_ui_1")
+ 
