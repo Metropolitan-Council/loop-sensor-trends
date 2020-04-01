17a63bda8a5bf7e83a9075bb3a4e1d3b2a23b022
Liz Roten <liz.roten@metc.state.mn.us>
2020-04-01 15:13:02 -0500

    add code for installing council.skeleton

--- covid.traffic.trends/dev/run_dev.R
+++ covid.traffic.trends/dev/run_dev.R
@@ -8,6 +8,8 @@ options(shiny.launch.browser = TRUE,
 # Detach all loaded packages and clean your environment
 # golem::detach_all_attached()
 # rm(list=ls(all.names = TRUE))
+# remove.packages("council.skeleton", lib="~/R/win-library/3.6")
+# remotes::install_github("Metropolitan-Council/council.skeleton", upgrade = "never")
 
 rmarkdown::render("./ABOUT.Rmd", output_format = "github_document", # -----
                   output_file = "ABOUT.md", output_dir = "./inst/app/www",
