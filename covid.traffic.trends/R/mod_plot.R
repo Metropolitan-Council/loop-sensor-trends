6252f17411ef2c50d63c3514b9dba718e0a19c5c
Liz Roten <liz.roten@metc.state.mn.us>
2020-04-23 09:27:28 -0500

    modify plot annotations width; update app data 4/23

--- covid.traffic.trends/R/mod_plot.R
+++ covid.traffic.trends/R/mod_plot.R
@@ -116,7 +116,7 @@ mod_plot_server <- function(input, output, session) {
       ) %>% 
       add_annotations(
         data = covid.traffic.trends::mn_actions,
-        text = stringr::str_wrap(covid.traffic.trends::mn_actions$action, width = 25),
+        text = stringr::str_wrap(covid.traffic.trends::mn_actions$action, width = 20),
         x = covid.traffic.trends::mn_actions$date,
         y = covid.traffic.trends::mn_actions$typical_vmt_diff,
         showarrow = TRUE,
